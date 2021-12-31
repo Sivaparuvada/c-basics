38a7aba6a45e209b7227a8d32a696d5f515be2c5
Sivaparuvada <96334378+Sivaparuvada@users.noreply.github.com>
2021-12-31 15:50:12 +0530

    Add files via upload

--- current bill.c
+++ current bill.c
@@ -10,9 +10,19 @@ void main()
 		amt=unit*1.20;
 		printf("amount of charge:%.2f\n",amt);
 	}
-	else if(unit>=200 && unit<=400)
+	else if(unit>=200 && unit<400)
 	{
 		amt=unit*1.50;
 		printf("amount of charge:%.2f\n",amt);
+	}
+	else if(unit>=400 && unit<600)
+	{
+		amt=unit*1.80;
+		printf("amount of charge:%.2f",amt);
+	} 
+	else if(unit>=600)
+	{
+		amt=unit*2;
+		printf("amount of charge:%.2f",amt);
 	}
 }
