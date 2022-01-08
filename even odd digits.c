d38d68b4336d984d34c384dc17c4d9c19d3e1deb
Sivaparuvada <96334378+Sivaparuvada@users.noreply.github.com>
2022-01-08 15:21:54 +0530

    Add files via upload

---
+++ even odd digits.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int n,r,edc=0,odc=0,zc=0;
+	scanf("%d",&n);
+	while(n>0)
+	{
+		r=n%10;
+		if(r==0)
+		{
+		   zc++;
+		}
+		else if(r%2==0)
+		{
+			edc++;
+		}
+		else
+		{
+			odc++;
+		}
+	    n=n/10;
+}
+printf("%d %d %d",zc,edc,odc);
+}
